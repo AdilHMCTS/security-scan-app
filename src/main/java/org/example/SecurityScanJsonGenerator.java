a54e03b7a19ad0c69963212edee5940a21a9658e
AdilOoze <adil.oozeerally@hmcts.net>
2021-05-18 17:30:34 +0100

    Adding app

---
+++ src/main/java/org/example/SecurityScanJsonGenerator.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SecurityScanJsonGenerator {
+
+
+    public static void main(String[] args) throws IOException, ParseException {
+        JSONArray jsonArray = (JSONArray) new JSONParser().parse(new FileReader("/Users/778425/Projects/security-scan-app/src/main/resources/test.json"));
+
+        System.out.println(jsonArray.size());
+
+        for (Object o : jsonArray) {
+
+            JSONObject jsonObject = (JSONObject) o;
+            String key = jsonObject.get("fingerprint").toString();
+            System.out.println("\"" + key + "\" : " + "\"ignore\",");
+
+
+        }
+
+
+    }
+}
